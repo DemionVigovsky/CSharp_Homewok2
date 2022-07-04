bb243468536b12e936ac15d4985ed2d0a45b0194
DemionVigovsky <morrow130790130@gmail.com>
2022-07-04 23:50:08 +0300

    Сделал Задачу 10 самым коротким способом

---
+++ Homework001_SecondNumber_Fromhundred/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = 782;
+Console.WriteLine(number);
+
+int secondNumber = (number / 10) % 10;
+int firstNumber = number / 100;
+int thirdNumber = number % 10;
+
+Console.WriteLine("вторая цифра числа - " + secondNumber);
