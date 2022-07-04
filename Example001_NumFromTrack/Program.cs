9db47500c5e8a727541cb2aa557c0908c6b46f1a
DemionVigovsky <morrow130790130@gmail.com>
2022-07-04 23:07:38 +0300

    Вывели макс число из рандомного двухначного числа

---
+++ Example001_NumFromTrack/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(10,99);
+Console.WriteLine(number);
+
+int secondnumber = number % 10;
+int firstnumber = number / 10;
+int max = 0;
+
+if(firstnumber > secondnumber) max = firstnumber;
+if(secondnumber > firstnumber) max = secondnumber;
+
+Console.WriteLine(max);
