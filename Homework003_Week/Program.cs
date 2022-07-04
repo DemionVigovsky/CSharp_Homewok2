5ad7c56b5a50a8e56fadf2347e5378eea41c373b
DemionVigovsky <morrow130790130@gmail.com>
2022-07-05 00:23:22 +0300

    Выполнил задание, но код некрасивый и топорный

---
+++ Homework003_Week/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число дня недели");
+int weekNumber = Convert.ToInt32(Console.ReadLine());
+
+if(weekNumber == 6 || weekNumber == 7)
+{
+    Console.WriteLine("Да, это выходной день");
+} 
+if (weekNumber < 1 || weekNumber > 7)
+{
+    Console.WriteLine ("Ошибка! Введите число от 1 до 7");
+}
+if(weekNumber == 1 || weekNumber == 2 || weekNumber == 3 || weekNumber == 4 || weekNumber == 5)
+    {
+    Console.WriteLine("Нет, это будний день");
+    }
+
