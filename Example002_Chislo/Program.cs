afb1015dca1ed352817d80f31f514f6af828d4d3
DemionVigovsky <morrow130790130@gmail.com>
2022-07-06 17:32:59 +0300

    Вывод второго числа из трехзначного

---
+++ Example002_Chislo/Program.cs
@@ -0,0 +1,19 @@
+﻿/*int numberA = new Random().Next(100,999);
+Console.WriteLine (numberA);
+int firstNumber = numberA / 100;
+int thirdNumber = numberA % 10;
+Console.WriteLine(firstNumber * 10 + thirdNumber);*/
+
+int secondNumber (int first, int third)
+{
+    int numberA = new Random().Next(first, third);
+    Console.WriteLine (numberA);
+    int firstNumber = numberA / 100;
+    int thirdNumber = numberA % 10;
+    int result = firstNumber * 10 + thirdNumber;
+    return result;
+}
+
+
+int A = secondNumber (100,999);
+Console.WriteLine (A);
