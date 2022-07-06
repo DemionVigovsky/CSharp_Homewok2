a809115a59237c47958abb18fead662233dc6018
DemionVigovsky <morrow130790130@gmail.com>
2022-07-06 19:39:10 +0300

    Кратное или некратное число с остатком

---
+++ Example003_TwoNum/Program.cs
@@ -0,0 +1,13 @@
+﻿void twoNumbers (int numberA, int numberB)
+{
+    if (numberA % numberB == 0)
+    {
+        Console.WriteLine ("Кратное");
+    }
+    else
+    {
+        Console.WriteLine ("Не кратное, остаток " + numberA % numberB);
+    }
+}
+
+twoNumbers (110, 21);
